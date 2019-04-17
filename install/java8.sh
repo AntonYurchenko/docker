cf1d02f608a03390a19603c7dfc4b72f107d3855
Anton <a.yurchenko@corp.mail.ru>
2019-04-17 19:06:42 +0300

    docker_scala_mxnet_1.4.0: Oracle java has been changed on open-jdk. Dockerfile for new version MXNet (1.4.0) has been added

--- install/java8.sh
+++ install/java8.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
 # Install Java 8
-apt-get install -y software-properties-common && \
-    add-apt-repository ppa:webupd8team/java -y && \
-    apt-get update && \
-    echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | debconf-set-selections && \
-    apt-get install -y oracle-java8-installer
+apt-get install -y openjdk-8-jdk
 
--- install/timeZone.sh
+++ install/timeZone.sh
@@ -5,6 +5,7 @@
 TIME_ZONE=${1}
 
 # Install TZData and set time zone
+export DEBIAN_FRONTEND=noninteractive
 apt-get install -y tzdata && \
     ln -sf /usr/share/zoneinfo/$TIME_ZONE /etc/localtime && \
     dpkg-reconfigure -f noninteractive tzdata
